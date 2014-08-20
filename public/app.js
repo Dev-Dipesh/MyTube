56914a7d0bf4354bbb8d0b5614f6ec761397af75
unknown <dipeshb@GISI-LT-Dipeesh.gisi.com>
2014-08-20 15:17:47 +0530

    Modular JSON Imlemented

--- public/app.js
+++ public/app.js
@@ -1,7 +1,7 @@
 (function () {
     "use strict";
     angular.module('tubeShow', [])
-        .controller('TubeController', function ($scope) {
+        .controller('TubeController', function ($scope, $http) {
             $scope.init = function () {
                 $scope.stage = new Kinetic.Stage({
                     container: 'myCanvas',
@@ -9,69 +9,35 @@
                     height: 768
                 });
 
-                $scope.categories = [
-                    {"id" : 1, "name": "SDLC",      "showName": "Development",  "tubes": ["Dev", "Test", "Arch", "Tool"]},
-                    {"id" : 2, "name": "DATA",      "showName": "Data",         "tubes": ["Dev", "BigD", "Data"]},
-                    {"id" : 3, "name": "TESTING",   "showName": "Testing",      "tubes": ["Dev", "Test", "Mobi"]},
-                    {"id" : 4, "name": "ORA",   "showName": "ORA",      "tubes": ["Dev", "Test", "Mobi"]},
-                ];
+                $http.get("/data/categories.json")
+                    .then(function(results){
+                        //Success
+                        $scope.categories = results.data;
+                        console.log("Passed: " + results.data);        
+                    }, function(results){
+                        //error
+                        console.log("Error: " + results.data + "; "+ results.status);
+                    });
 
-                $scope.tubes = {
-                    "Dev":  {"ipath" : "images/Ind-Dev.png", "xoff": 90, "yoff": 10, "xsiz": "", "ysiz": ""},
-                    "Test": {"ipath" : "images/Ind-Test.png", "xoff": 600, "yoff": 120, "xsiz": "", "ysiz": ""},
-                    "Arch": {"ipath" : "images/Ind-Arch.png", "xoff": 160, "yoff": 400, "xsiz": "", "ysiz": ""},
-                    "Mobi": {"ipath" : "images/Ind-Mobility.png", "xoff": 285, "yoff": 80, "xsiz": "", "ysiz": ""},
-                    "Mtnc": {"ipath" : "images/Ind-Maint.png", "xoff": 560, "yoff": 447, "xsiz": "", "ysiz": ""},
-                    "Data": {"ipath" : "images/Ind-DataRef.png", "xoff": 0, "yoff": 130, "xsiz": "", "ysiz": ""},
-                    "BigD": {"ipath" : "images/Ind-BigD.png", "xoff": 345, "yoff": 118, "xsiz": "", "ysiz": ""},
-                    "Tool": {"ipath" : "images/Ind-ToolsFr.png", "xoff": 128, "yoff": 68, "xsiz": "", "ysiz": ""}
-                };
-                /*
-                * Hotspots are a list of areas where hyper links are to be provided for each image (if any)
-                * Suitable SVG shapes (circle/ rect) are drawn out dynamically. 
-                * LHS key is source and Target (to) is within RHS (value) JSON. Required Source+Target to be Unique. 
-                * Internal link means overlaying another image (tube map) at a predefined location.
-                * URL means a local file or a web link or a presentation / media about the offering
-                * All information is the form JSON
-                */
-                $scope.hotSpots = {
-                    "Dev": [
-                        {"x": 240, "y": 40,  "link": {"type": "internal", "to": "Test"}, "hotspot": {"shape": "circle", "radius": 30}},
-                        {"x": 232, "y": 699, "link": {"type": "internal", "to": "Arch"}, "hotspot": {"shape": "circle", "radius": 30}},
-                        {"x": 575, "y": 543, "link": {"type": "internal", "to": "Mtnc"}, "hotspot": {"shape": "circle", "radius": 30}},
-                        {"x": 117, "y": 455, "link": {"type": "internal", "to": "Tool"}, "hotspot": {"shape": "circle", "radius": 20}},
-                        {"x": 465, "y": 700, "link": {"type": "url", "to": "http://www.genisys-group.com/consulting.html"},
-                            "hotspot": {"shape": "circle", "radius": 30}},
-                        {"x": 418, "y": 33, "link": {"type": "url", "to": "resources/SoftwareServicesGenisys.pdf"},
-                            "hotspot": {"shape": "rect", "height": 30, "width": 160}}
-                    ],
-                    "Test": [
-                        {"x": 895, "y": 393, "link": {"type": "internal", "to": "Mobi"}, "hotspot": {"shape": "circle", "radius": 20}},
-                        {"x": 625, "y": 255, "link": {"type": "url", "to": "http://www.genisys-group.com/itapp_assurance_services.html"},
-                                "hotspot": {"shape": "rect", "height": 20, "width": 60}}
-                    ],
-                    "Mobi": [
-                        {"x": 675, "y": 335, "link": {"type": "url", "to": "http://www.genisys-group.com/mobility"},
-                            "hotspot": {"shape": "rect", "height": 20, "width": 60}}
-                    ],
-                    "Mtnc": [
-                        {"x": 692, "y": 488, "link": {"type": "url", "to": "http://www.genisys-group.com/application_management.html"},
-                            "hotspot": {"shape": "rect", "height": 20, "width": 150}}
-                    ],
-                    "Arch": [
-                        {"x": 255, "y": 467, "link": {"type": "internal", "to": "Tool"}, "hotspot": {"shape": "rect", "height": 20, "width": 100}},
-                        {"x": 230, "y": 472, "link": {"type": "internal", "to": "Data"}, "hotspot": {"shape": "circle", "radius": 20}}
-                    ],
-                    "Data": [
-                        {"x": 246, "y": 283, "link": {"type": "internal", "to": "BigD"}, "hotspot": {"shape": "circle", "radius": 15}},
-                        {"x": 255, "y": 467, "link": {"type": "internal", "to": "Tool"}, "hotspot": {"shape": "rect", "height": 20, "width": 100}}
-                    ],
-                    "Tool": [
-                        {"x": 685, "y": 207, "link": {"type": "internal", "to": "Test"}, "hotspot": {"shape": "circle", "radius": 15}}
-                    ]
-                };
-                    //$scope.loadTube('Dev');
-                    //$scope.stage.batchDraw();
+                $http.get("/data/tube.json")
+                    .then(function(results){
+                        //Success
+                        $scope.tubes = results.data;
+                        console.log("Passed: " + results.data);        
+                    }, function(results){
+                        //error
+                        console.log("Error: " + results.data + "; "+ results.status);
+                    });
+
+                $http.get("/data/hotspots.json")
+                    .then(function(results){
+                        //Success
+                        $scope.hotSpots = results.data;
+                        console.log("Passed: " + results.data);        
+                    }, function(results){
+                        //error
+                        console.log("Error: " + results.data + "; "+ results.status);
+                    });
             };  //init ends
 
             $scope.loadTube = function (tube) {
